c52cda8953b0147067b85886b81ab9e54c562476
thoibi3 <thoibi3@gmail.com>
2025-07-08 20:43:42 +0530

    fix

--- var.tf
+++ var.tf
@@ -1,6 +1,6 @@
 variable "s3_main_bucket" {
-  type        = string
-  default     = "Demo.s3.newbucket"
+  type    = string
+  default = "Demo.s3.newbucket"
 }
 
 variable "lambda_iam_role_name" {
@@ -16,8 +16,8 @@ variable "s3_lamda_trigger_name" {
 }
 
 variable "functionname" {
-  type        = string  
-  default     = "testlamda"
+  type    = string
+  default = "testlamda"
 }
 
 
