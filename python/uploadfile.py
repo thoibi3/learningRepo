9bc211f46ae2c7541931cb2cf5c6c49477411162
thoibi3 <thoibi3@gmail.com>
2025-07-08 11:57:58 +0530

    fix it

--- python/uploadfile.py
+++ python/uploadfile.py
@@ -3,7 +3,7 @@ import logging
 
 bucket_name = 'raja.s3.newbucket'
 file_path = 'test_file.txt'             # Local file to upload
-s3_key = 'uploads/test_file.txt'        # Path in S3
+s3_key = 'uploads/test_file.txt'        # Path in S3 S3
 
 
 # Create an S3 client
@@ -11,7 +11,7 @@ s3 = boto3.client('s3')
 
 try:
     s3.upload_file(file_path, bucket_name, s3_key)
-    print(f"File uploaded successfully to s3://{bucket_name}/{s3_key}")
+    print(f"File uploaded successfullyy to s3://{bucket_name}/{s3_key}")
     logging.warning("Remain calm!")
 except Exception as e:
     print(f"Upload failed: {e}")
