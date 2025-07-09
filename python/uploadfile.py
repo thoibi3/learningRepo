49e20f4b663fd3fe861226f4ed2b9feeb8103fd0
thoibi3 <thoibi3@gmail.com>
2025-07-09 07:28:41 +0530

    ssdsd

--- python/uploadfile.py
+++ python/uploadfile.py
@@ -12,7 +12,7 @@ s3 = boto3.client('s3')
 
 try:
     s3.upload_file(file_path, bucket_name, s3_key)
-    print(f"File uploaded successfully to s3://{bucket_name}/{s3_key}")
+    print(f"File uploaded successfullyy to s3://{bucket_name}/{s3_key}")
     logging.warning("Remain calm!")
 except Exception as e:
     print(f"Upload failed: {e}")
